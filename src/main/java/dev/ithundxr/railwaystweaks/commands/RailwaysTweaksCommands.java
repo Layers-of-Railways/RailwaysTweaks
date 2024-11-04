2405887e6d0fe90838f3476d8d282dd6f2416b84
IThundxr <me@ithundxr.dev>
2024-11-03 21:55:49 -0500

    dump trains cmd

--- src/main/java/dev/ithundxr/railwaystweaks/commands/RailwaysTweaksCommands.java
+++ src/main/java/dev/ithundxr/railwaystweaks/commands/RailwaysTweaksCommands.java
@@ -1,6 +1,8 @@
 package dev.ithundxr.railwaystweaks.commands;
 
 import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.simibubi.create.Create;
+import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
 import dev.ithundxr.railwaystweaks.mixin.compat.tconstruct.SimpleChannelAccessor;
 import me.pepperbell.simplenetworking.C2SPacket;
 import me.pepperbell.simplenetworking.S2CPacket;
@@ -22,12 +24,23 @@ public class RailwaysTweaksCommands {
             dispatcher.register(literal("railwaystweaks")
                 .then($dump_hephaestus_packets()));
         });
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(literal("railwaystweaks")
+                    .then($dump_trains()));
+        });
     }
 
     private static ArgumentBuilder<CommandSourceStack, ?> $dump_hephaestus_packets() {
         return literal("dump_hephaestus_packets")
             .requires(cs -> cs.hasPermission(2))
             .executes(ctx -> dumpHephaestusPackets(ctx.getSource()));
+    }
+
+    private static ArgumentBuilder<CommandSourceStack, ?> $dump_trains() {
+        return literal("dump_create_trains")
+                .requires(cs -> cs.hasPermission(2))
+                .executes(ctx -> dumpCreateTrains(ctx.getSource()));
     }
 
     private static int dumpHephaestusPackets(CommandSourceStack source) {
@@ -61,6 +74,26 @@ public class RailwaysTweaksCommands {
         });
 
         source.sendSuccess(() -> Component.literal(sb.toString()), true);
+
+        return 0;
+    }
+
+    private static int dumpCreateTrains(CommandSourceStack source) {
+        StringBuilder s = new StringBuilder();
+
+        Create.RAILWAYS.trains.forEach((uuid, train) -> {
+            s.append("Train Name: ").append(train.name.toString());
+
+            CarriageContraptionEntity entity = train.carriages.get(0).anyAvailableEntity();
+
+            if (entity != null) {
+                s.append(", is at ").append(entity.position());
+            }
+
+            s.append("\n");
+        });
+
+        source.sendSuccess(() -> Component.literal(s.toString()), true);
 
         return 0;
     }
