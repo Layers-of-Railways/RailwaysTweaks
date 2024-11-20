64c4dd7a4980085bf7accd0e71724ce776242688
IThundxr <me@ithundxr.dev>
2024-11-19 21:28:48 -0500

    Let's try this again, shall we?

--- src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/create/BlockHelperMixin.java
+++ src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/create/BlockHelperMixin.java
@@ -4,13 +4,15 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.foundation.utility.BlockHelper;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockHelper.class)
 public class BlockHelperMixin {
-    @ModifyExpressionValue(method = "placeSchematicBlock", at = @At(value = "INVOKE", target = "Ljava/lang/Object;equals(Ljava/lang/Object;)Z"))
-    private static boolean railwayTweaks$fixSchematicannonCrash(boolean original, @Local(ordinal = 1) BlockEntity loaded) {
-        return loaded != null && original;
+    @Redirect(method = "placeSchematicBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BlockEntity;getType()Lnet/minecraft/world/level/block/entity/BlockEntityType;", ordinal = 1))
+    private static BlockEntityType<?> railwayTweaks$fixSchematicannonCrash(BlockEntity instance, @Local(ordinal = 1) BlockEntity loaded) {
+        return loaded != null ? instance.getType() : null;
     }
 }
