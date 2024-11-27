5a6fbb2cd51a935b5b55c7842d42f657f33c2f00
Sleepy Evelyn <46009144+sleepy-evelyn@users.noreply.github.com>
2024-11-27 17:42:04 +0000

    Suppress Tinkers Modifier exception

---
+++ src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/tconstruct/LazyModifierMixin.java
@@ -0,0 +1,29 @@
+package dev.ithundxr.railwaystweaks.mixin.compat.tconstruct;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.modifiers.ModifierId;
+import slimeknights.tconstruct.library.modifiers.ModifierManager;
+import slimeknights.tconstruct.library.modifiers.util.LazyModifier;
+
+@Mixin(LazyModifier.class)
+public class LazyModifierMixin {
+
+    @Shadow @Final protected ModifierId id;
+
+    @Inject(
+            method = "get()Lslimeknights/tconstruct/library/modifiers/Modifier;",
+            at = @At("HEAD"),
+            cancellable = true,
+            remap = false
+    )
+    private void railwayTweaks$suppressException(CallbackInfoReturnable<Modifier> cir) {
+        if (!ModifierManager.INSTANCE.isDynamicModifiersLoaded())
+            cir.setReturnValue(ModifierManager.INSTANCE.getDefaultValue());
+    }
+}
