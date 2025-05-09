37a84ac3cf3a472a4a370780291a59188c05aeb9
NaomiNSFW <46009144+sleepy-evelyn@users.noreply.github.com>
2025-05-09 21:38:34 +0100

    Increase recipe backup check interval

---
+++ src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/create/BasinBlockEntityMixin.java
@@ -0,0 +1,30 @@
+package dev.ithundxr.railwaystweaks.mixin.compat.create;
+
+import com.simibubi.create.content.processing.basin.BasinBlockEntity;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+
+@Mixin(BasinBlockEntity.class)
+public class BasinBlockEntityMixin {
+
+    @Unique @Final int railwayTweaks$BACKUP_CHECK_INTERVAL = 120;
+
+    @Shadow int recipeBackupCheck;
+
+    @Redirect(
+            method = "lazyTick",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lcom/simibubi/create/content/processing/basin/BasinBlockEntity;recipeBackupCheck:I",
+                    ordinal = 1,
+                    opcode = Opcodes.PUTFIELD
+            )
+    )
+    private void railwayTweaks$changeRecipeBackupCheckInterval(BasinBlockEntity bbe, int recipeBackupCheck) {
+        this.recipeBackupCheck = railwayTweaks$BACKUP_CHECK_INTERVAL;
+    }
+}
