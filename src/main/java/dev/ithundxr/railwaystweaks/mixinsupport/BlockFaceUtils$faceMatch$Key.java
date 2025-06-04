317c3c3e5a661041682ca4d2506a170ffe5d17e0
techno-sam <77073745+techno-sam@users.noreply.github.com>
2025-06-04 09:46:43 +0200

    remove illegal caching

--- src/main/java/dev/ithundxr/railwaystweaks/mixinsupport/BlockFaceUtils$faceMatch$Key.java
+++
@@ -1,6 +0,0 @@
-package dev.ithundxr.railwaystweaks.mixinsupport;
-
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.state.BlockState;
-
-public record BlockFaceUtils$faceMatch$Key(long fromPos, long toPos, BlockState fromState, BlockState toState, Direction fromFace) {}
