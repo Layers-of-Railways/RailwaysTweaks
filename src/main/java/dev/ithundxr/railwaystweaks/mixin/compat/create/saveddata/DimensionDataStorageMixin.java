ce48295dfdac172bc72e707173639d387b00052e
IThundxr <me@ithundxr.dev>
2025-07-01 12:49:43 -0400

    Revert "Fix logging"
    This reverts commit f2ec4e420b5a658b9452000c0b7a4eba131ccc75.

--- src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/create/saveddata/DimensionDataStorageMixin.java
+++ src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/create/saveddata/DimensionDataStorageMixin.java
@@ -49,8 +49,7 @@ public abstract class DimensionDataStorageMixin {
 	
 	@WrapOperation(method = "readTagFromDisk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/DimensionDataStorage;getDataFile(Ljava/lang/String;)Ljava/io/File;"))
 	private File railwaysTweaks$fixGetDataFile(DimensionDataStorage instance, String name, Operation<File> original) {
-		boolean old = name.endsWith("_old");
-		if (old) RailwaysTweaks.LOGGER.info("Reading .dat_old file for {}", name);
-		return old ? new File(this.dataFolder, name) : original.call(instance, name);
+		RailwaysTweaks.LOGGER.info("Reading .dat_old file for {}", name);
+		return name.endsWith("_old") ? new File(this.dataFolder, name) : original.call(instance, name);
 	}
 }
