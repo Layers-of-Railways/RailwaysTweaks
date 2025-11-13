d2a620f8b2aabeee4561a4f10067b4ca0951c6cb
sleepy-evelyn <46009144+sleepy-evelyn@users.noreply.github.com>
2025-11-13 17:50:47 +0000

    Replace bad Train Mixin code

--- src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/create/TrainMixin.java
+++ src/main/java/dev/ithundxr/railwaystweaks/mixin/compat/create/TrainMixin.java
@@ -1,37 +1,36 @@
 package dev.ithundxr.railwaystweaks.mixin.compat.create;
 
-import com.simibubi.create.content.trains.entity.Carriage;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.trains.entity.Train;
 import com.simibubi.create.foundation.utility.Pair;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = Train.class, remap = false)
-public class TrainMixin {
+@Mixin(value = Train.class, priority = 1000000, remap = false)
+public abstract class TrainMixin {
 
-    @ModifyConstant(
-            method = "updateNavigationTarget",
-            constant = @Constant(intValue = 100)
-    )
-    private int increaseFullRefreshDistance(int original) {
-        return 200; // Reduce navigation calls by increasing the distance before a new calculation is made
-    }
-
-    @Redirect(
-            method="collideWithOtherTrains",
+    @WrapOperation(
+            method = "collideWithOtherTrains",
             at=@At(
                     value = "INVOKE",
                     target = "Lcom/simibubi/create/content/trains/entity/Train;findCollidingTrain(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/resources/ResourceKey;)Lcom/simibubi/create/foundation/utility/Pair;"
             )
     )
-    private Pair<Train, Vec3> railwayTweaks$removeTrainCollisions(Train instance, Level otherLeading, Vec3 otherTrailing, Vec3 otherDimension, ResourceKey<Level> start2) {
+    private Pair<Train, Vec3> railwayTweaks$removeTrainCollisions(Train instance, Level otherLeading, Vec3 otherTrailing, Vec3 otherDimension, ResourceKey<Level> start2, Operation<Pair<Train, Vec3>> original) {
         return null;
+    }
+
+    @ModifyExpressionValue(
+            method = "updateNavigationTarget",
+            at = @At(value = "CONSTANT", args = "intValue=100")
+    )
+    private int increaseFullRefreshDistance(int original) {
+        // Reduce navigation calls by increasing the distance before a new full calculation is made
+        return 200;
     }
 }
